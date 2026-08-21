95dc59d485b82a5370ad8a887e7a57f27160468b
hridyanshjoshi780svg <hridyanshjoshi780@gmail.com>
2026-08-21 16:31:34 +0530

    10 more logical questions to practice python =

--- hello.py
+++ hello.py
@@ -1,13 +1,5 @@
-num1 = int(input("enter a number: "))
-num2 = int(input("enter a number: "))
-sign = input("enter + - * / any of these: ")
-if sign == '+':
-    print(num1+num2)
-elif sign == '-':
-    print(num1-num2)
-elif sign == '*':
-    print(num1*num2)
-elif sign == '/':
-    print(num1/num2)
-else:
-    print("invalid sign try again!")
+numbers = [10, -5, 20, -3, 30]
+for i in range(len(numbers)):
+    if numbers[i] < 0:
+        continue
+    print(numbers[i])
