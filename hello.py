fed98a6eeffd7e0a5444b86454a74611f5681290
hridyanshjoshi780svg <hridyanshjoshi780@gmail.com>
2026-08-27 10:13:53 +0530

    file handling

--- hello.py
+++ hello.py
@@ -1,5 +1,3 @@
-numbers = [10, -5, 20, -3, 30]
-for i in range(len(numbers)):
-    if numbers[i] < 0:
-        continue
-    print(numbers[i])
+file = open("/home/hridyansh/Documents/Monday.md")
+print(file.read())
+file.close()
